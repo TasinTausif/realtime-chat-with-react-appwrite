0af7c34cb9f79ca05749c27a5be7e0687f9e331d
Tasin Tausif <128625424+TasinTausif@users.noreply.github.com>
2026-05-13 22:54:26 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    'import.meta.env.VITE_APPWRITE_ENDPOINT': JSON.stringify(process.env.VITE_APPWRITE_ENDPOINT),
+    'import.meta.env.VITE_APPWRITE_PROJECT_ID': JSON.stringify(process.env.VITE_APPWRITE_PROJECT_ID),
+    'import.meta.env.VITE_APPWRITE_DATABASE': JSON.stringify(process.env.VITE_APPWRITE_DATABASE),
+    'import.meta.env.VITE_APPWRITE_COLLECTION': JSON.stringify(process.env.VITE_APPWRITE_COLLECTION),
+  },
+  server: {
+    host: true
+  }
 })
